125593130c36cb82b5cad8f92039771f1e7fd23a
Sandra de la Torre <delatorremartinezsandra@gmail.com>
2025-03-28 09:14:49 +0100

    My first commit

--- angular-frontend/src/app/modules/tasks/tasks.component.ts
+++ angular-frontend/src/app/modules/tasks/tasks.component.ts
@@ -3,16 +3,29 @@ import { CommonModule } from '@angular/common';
 import { TaskService } from '../../shared/services/task-service.service';
 import { Task } from '../../shared/interfaces/task';
 import { RouterModule } from '@angular/router';
-
-
+import {
+  FormControl,
+  ReactiveFormsModule,
+  Validator,
+  Validators,
+} from '@angular/forms';
+import { ErrorMessageComponent } from './error-message/error-message.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ErrorMessageComponent,
+  ],
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  title = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  describe = new FormControl('', Validators.required);
+
   tasks: Task[] = [];
 
   constructor(private taskService: TaskService) {}
@@ -23,17 +36,17 @@ export class TasksComponent implements OnInit {
 
   loadTasks(): void {
     this.taskService.getTasks().subscribe(
-      (tasks) => this.tasks = tasks,
+      (tasks) => (this.tasks = tasks),
       (error) => console.error('Error fetching tasks:', error)
     );
   }
 
   addTask(title: string): void {
-    if (!title.trim()) return;
+    if (this.title.invalid || this.describe.invalid) return;
+
     const newTask: Task = { id: 0, title, completed: false };
     this.taskService.addTask(newTask).subscribe(() => this.loadTasks());
   }
-  
 
   toggleTask(task: Task): void {
     task.completed = !task.completed;
