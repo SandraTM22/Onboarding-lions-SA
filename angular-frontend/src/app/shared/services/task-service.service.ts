59eb83d36496001d2c7209d8d3322bce56df3bbf
Sandra de la Torre <delatorremartinezsandra@gmail.com>
2025-03-28 11:16:10 +0100

    Creacion de barra busqueda en vista

--- angular-frontend/src/app/shared/services/task-service.service.ts
+++ angular-frontend/src/app/shared/services/task-service.service.ts
@@ -8,9 +8,14 @@ import { Task } from '../interfaces/task';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:8000/api/tasks';
+  private apiUrl = 'http://localhost:8000/api/tasks'; //Apunta al backend(entrada de los datos)
 
   constructor(private http: HttpClient) {}
+
+  //http.get<Task[]>(this.apiUrl): Llama a la URL de la API (this.apiUrl), 
+  //que devuelve todas las tareas en formato JSON. El tipo Task[] nos dice que esperamos una lista de tareas.
+
+  //Observable:  emitirá las tareas cuando la petición se complete. Esto permite trabajar con los datos de manera asíncrona
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
