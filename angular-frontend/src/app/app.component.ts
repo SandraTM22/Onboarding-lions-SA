85512b0f9bdef60e01bb683f4fccf1cb4e7b519e
Sandra de la Torre <delatorremartinezsandra@gmail.com>
2025-04-01 09:06:17 +0200

    Creación toast.Manejo submit crear tarea

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastComponent } from './shared/toast/toast.component';
+import { CommonModule } from '@angular/common';
+import { ToastService } from './shared/services/toast.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ToastComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-frontend';
+
+  constructor(private toastService: ToastService) {}
+
+  showSuccessToast() {
+    this.toastService.addToast('success', 'Successful operation');
+  }
+
+  showErrorToast() {
+    this.toastService.addToast('error', 'Error: An error has occurred');
+  }
+
+  showInfoToast() {
+    this.toastService.addToast('info', 'This is an information message');
+  }
+  showWarnToast() {
+    this.toastService.addToast('warning', 'Warning: warning message');
+  }
 }
