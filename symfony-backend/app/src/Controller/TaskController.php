c13e240ebeaf665de240f03adf6667e4c2f240b8
Sandra de la Torre <delatorremartinezsandra@gmail.com>
2025-04-03 08:43:15 +0200

    Intento modificación campos BBDD. Creacion controller)

--- symfony-backend/app/src/Controller/TaskController.php
+++ symfony-backend/app/src/Controller/TaskController.php
@@ -73,4 +73,29 @@ class TaskController extends AbstractController
             'completed' => $task->getCompleted()
         ]);
     }
+
+    #[Route('', name: 'update_complete_task', methods: ['PUT'])]
+    public function update_complete(EntityManagerInterface $em): JsonResponse
+    {
+        //Buscamos las tareas
+        $tasks = $em->getRepository(Task::class)->findAll();
+        if (empty($tasks)) {
+
+            return $this->json(['error' => 'Tasks not found'], 404);
+        }
+        //Cambiamos el campo completed a true de cada tarea
+        foreach ($tasks as $task) {
+            $task->setCompleted("true");
+        }
+        //Guardamos en la bbdd
+        $em->flush();
+        //Recogemos todos los datos de cada tarea
+        $data = array_map(fn($task) => [
+            'id' => $task->getId(),
+            'title' => $task->getTitle(),
+            'completed' => $task->getCompleted()
+        ], $tasks);
+        //retornamos la iformación antes recogida en un JSON
+        return $this->json($data);
+    }
 }
