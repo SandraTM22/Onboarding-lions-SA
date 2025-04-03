fa2eadc5ad247b16b004270f583eda321131b8dd
Sandra de la Torre <delatorremartinezsandra@gmail.com>
2025-04-03 11:12:20 +0200

    Creacion QueryBBDD y endpoint correspondiente

--- symfony-backend/app/src/Controller/TaskController.php
+++ symfony-backend/app/src/Controller/TaskController.php
@@ -98,4 +98,24 @@ class TaskController extends AbstractController
         //retornamos la iformación antes recogida en un JSON
         return $this->json($data);
     }
+
+    #[Route('/sort', name: 'sort_list', methods: ['GET'])]
+    public function sort_list(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $param = $request->query->get('sort');
+        
+        //tendria que controlar si no se pasa nada por parametro...
+        if (!in_array(strtoupper($param), ["ASC", "DESC"])) {
+            return new JsonResponse(['error' => 'Invalid sort direction'], 400);
+        }
+        $tasks = $em->getRepository(Task::class)->sortList($param);
+
+        $data = array_map(fn($task) => [
+            'id' => $task->getId(),
+            'title' => $task->getTitle(),
+            'completed' => $task->getCompleted()
+        ], $tasks);
+        return $this->json($data);
+    }
+
 }
--- symfony-backend/app/src/Repository/TaskRepository.php
+++ symfony-backend/app/src/Repository/TaskRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Task;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * @extends ServiceEntityRepository<Task>
@@ -14,6 +15,14 @@ class TaskRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Task::class);
+    }
+
+    public function sortList(string $sort)
+    {
+        $qb = $this->createQueryBuilder('t')
+            ->orderBy('t.id', $sort);
+        $query = $qb->getQuery();
+        return $query->getResult();
     }
 
     //    /**
