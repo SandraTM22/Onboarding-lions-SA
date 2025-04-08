33e7d5f8f2e24b52190cda62534e5acd03f4cd17
Sandra de la Torre <delatorremartinezsandra@gmail.com>
2025-04-08 08:32:17 +0200

    Delete comments

--- angular-frontend/src/app/modules/tasks/tasks.component.ts
+++ angular-frontend/src/app/modules/tasks/tasks.component.ts
@@ -116,7 +116,7 @@ export class TasksComponent implements OnInit {
 
   updateTask(task: Task): void {
     //clonamos la tarea
-    this.taskBeingEdited = { ...task };    
+    this.taskBeingEdited = { ...task };        
   }
 
   saveEdit(): void {
